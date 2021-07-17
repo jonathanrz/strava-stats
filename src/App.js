7067c0686f9972282065d3a43f4644ddcf9f73cb
Jonathan Zanella <jonathanrafaelz@gmail.com>
2021-07-17 11:45:52 -0300

    strava login link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -23,7 +23,7 @@ function App() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Strava Stats
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Link color="inherit" href='http://www.strava.com/oauth/authorize?client_id=68485&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read'>Login</Link>
         </Toolbar>
       </AppBar>
     </Box>
