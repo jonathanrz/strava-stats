b4fe64bc2674a2b8047c5c1cbbee956abb25e43e
Jonathan Zanella <jonathanrafaelz@gmail.com>
2021-07-19 07:24:05 -0300

    use hash router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import duration from "dayjs/plugin/duration";
 import dayjs from "dayjs";
 import HomePage from "./HomePage";
